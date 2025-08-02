825925ec111e6b1813a0bb8bf8c5eb9d96342490
tmddjs <f22rapto3562@gmail.com>
2025-08-02 16:54:30 +0900

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = parseInt(process.env.PORT || '3562', 10);
   const listenOptions = {
     port,
     host: "0.0.0.0",
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
   try {
     // `reusePort` is not supported on some platforms (e.g. macOS).
     // Attempt to enable it, but fall back if the OS rejects the option.
-    server.listen({ ...listenOptions, reusePort: true }, () => {
+    server.listen({ ...listenOptions, reusePnort: true }, () => {
       log(`serving on port ${port}`);
     });
   } catch (err: any) {
